e2b46dc54c42ca3c363be6250c446a79be7bbcf9
koudaisano <koukuncreator@gmail.com>
2025-07-29 05:00:02 +0900

    Headerからmainタグへ変更

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <header className="App-header">
+      <main>
         <Post />
-      </header>
+      </main>
     </div>
   );
 }
