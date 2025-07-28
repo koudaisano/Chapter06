e7ccf7195eaaf2cba2e95d7803d1a5cddc805b99
koudaisano <koukuncreator@gmail.com>
2025-07-29 05:02:47 +0900

    インデント修正

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import { header } from '../data/header';
 import styles from './Header.module.css';
 
 const Header = () => {
-    return (
-        <header className={styles.header}>
-            <a className={styles.a} href='/'>{header.title}</a>
-            <a className={styles.a} href='/'>{header.titleEn}</a>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <a className={styles.a} href='/'>{header.title}</a>
+      <a className={styles.a} href='/'>{header.titleEn}</a>
+    </header>
+  );
 };
 
 export default Header;
